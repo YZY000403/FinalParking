bd62fe2e2ad93e170cf9e18fcb5b7f7dafb5a1e3
ziqiy2 <ziqiy2@illinois.edu>
2020-04-26 15:57:30 -0500

    commit 1

--- app/src/main/java/com/example/cs125final/LaunchActivity.java
+++ app/src/main/java/com/example/cs125final/LaunchActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,8 +19,13 @@ public class LaunchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
         Button login = findViewById(R.id.Login);
-        createSignInIntent();
-        login.setOnClickListener(v -> createSignInIntent());
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else {
+            createSignInIntent();
+            login.setOnClickListener(v -> createSignInIntent());
+        }
     }
 
     /**
--- app/src/main/java/com/example/cs125final/MainActivity.java
+++ app/src/main/java/com/example/cs125final/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.cs125final;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,11 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button search = findViewById(R.id.reply);
-        search.setOnClickListener(unused
-                -> nothing());
-        startActivity(new Intent(this, ReplyActivity.class));
-        finish();
+       // Button search = findViewById(R.id.Result);
+       // search.setOnClickListener(unused
+                //-> nothing());
+       // startActivity(new Intent(this, ReplyActivity.class));
+        //finish();
+        TextView it = findViewById(R.id.textView);
+        it.setText();
     }
     public void nothing() {
         int nothing1 = 1;
